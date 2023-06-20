25fae1e02e9def2b117f2285dd1a4463b507e5b1
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-20 16:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,29 @@ function convertToRoman(num) {
     };
 
   //your code here
+if (isNaN(num) || num < 0 || num > 100000) {
+    return "Invalid input. Please provide a number between 0 and 100000.";
+  }
 
+  let romanNumeral = '';
+
+  for (let i = 0; i < 7; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
+
+    while (num >= value) {
+      romanNumeral += symbol;
+      num -= value;
+    }
+  }
+
+  return romanNumeral;
 }
+
+// console.log(convertToRoman(36)); // Output: XXXVI
+	
+
+// }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
